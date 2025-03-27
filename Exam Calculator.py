07e3c9d6b35209859f168275a726fd45e23a6047
Raihan162-star <raihanisrarmuhammad03@gmail.com>
2025-03-27 13:41:02 +0000

    Add files via upload

---
+++ Exam Calculator.py
@@ -0,0 +1,28 @@
+
+print("Exam Calc")
+
+Subject = input("Subject name: ")
+MaxScore = int(100)
+Score = int(input("Students score:"))
+Grade = (MaxScore * Score / 100)
+
+if Grade >= 90:
+    print("A+")
+if Grade >= 80:
+    print("A-")
+if Grade >= 70:
+    print("B")
+if Grade >= 60:
+    print("C")
+if Grade >= 50:
+    print("D")
+else:
+    print("Loserr")
+   
+
+print("Made By Raihan israr Muhammad")
+
+
+
+
+
